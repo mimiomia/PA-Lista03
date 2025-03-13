b9baa426273c8a4f316b5b6a0223b1361818e8ac
mimiomia <112013959+mimiomia@users.noreply.github.com>
2025-03-12 21:21:28 -0300

    Update L3E05.py
    Melhoria do ex 5

--- L3E05.py
+++ L3E05.py
@@ -1,9 +1,7 @@
 a = int(input('Digite um valor A: '))
 b = int(input('Digite um valor B: '))
 
-c = a + b
-
-if c < 20:
+if a + b < 20:
     a = a * 10
     b = b * 10
     print('Os valores inseridos multiplicados por 10 são:', a , 'e', b)
